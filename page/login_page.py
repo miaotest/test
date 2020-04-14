5b4dd9d1dfadfe057955d208288b31fa4dc237a8
miao <2282484378@qq.com>
2020-04-14 12:03:17 +0800

    mine、login文件的提交

--- page/login_page.py
+++ page/login_page.py
@@ -1,3 +1,19 @@
 """
 登录页面
 """
+import page
+from base.base_page import BasePage
+
+
+class LoginPage(BasePage):
+    def input_user(self, user):  # 输入用户名
+        self.input_func(self.find_element_func(page.user), user)
+
+    def input_pwd(self, pwd):  # 输入密码
+        self.input_func(self.find_element_func(page.pwd), pwd)
+
+    def click_login(self):  # 登录点击
+        self.click_func(self.find_element_func(page.login_btn))
+
+    def click_confirm(self):  # 登录提示消息确认
+        self.click_func(self.find_element_func(page.confirm_btn))
--- page/mine_page.py
+++ page/mine_page.py
@@ -1,3 +1,10 @@
 """
 我的页面
 """
+import page
+from base.base_page import BasePage
+
+
+class MinePage(BasePage):
+    def click_login(self):
+        self.click_func(self.find_element_func(page.login))
