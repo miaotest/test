3329a6209f6f6c017f5f7ff03b1c83f2322283f9
miao <2282484378@qq.com>
2020-04-14 18:49:21 +0800

    basepage和testcase 未登录测试

--- base/base_page.py
+++ base/base_page.py
@@ -1,3 +1,7 @@
+"""
+PO基类
+"""
+from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 
 
@@ -20,3 +24,14 @@ class BasePage(object):
     def input_func(self, element, text):
         element.clear()
         element.send_keys(text)
+
+    def get_toast_func(self, text):
+        """获取toast信息方法"""
+        # 处理传入进来的定位信息
+        # xpath = '//*[contains(@text, "再次点击")]' # 文本值不能写死
+        xpath = By.XPATH, '//*[contains(@text, "{}")]'.format(text)
+        element = self.find_element_func(xpath)  # 调用当前类中的元素定位方法
+        return element.text  # 返回目标元素的text属性值
+
+        # element = WebDriverWait(driver, 3, .3). \
+        #     until(lambda x: x.find_element_by_xpath('//*[contains(@text, "再次点击")]'))
--- case/test_case.py
+++ case/test_case.py
@@ -1,3 +1,4 @@
+import allure
 import pytest
 
 from page.page_factory import PageFactory
@@ -6,26 +7,40 @@ from utils import get_driver
 
 class TestLogin(object):
     @pytest.fixture()
-    def before_func(self): # 相当于setup()
+    def before_func(self):  # 相当于setup()
         self.driver = get_driver()
         self.pagefactory = PageFactory(self.driver)  # 实例化统一入口类
         yield
         self.driver.quit()
 
     def test_func(self):  # 实例化页面操作
-        self.pagefactory.index_page().click_mine()
-        self.pagefactory.mine_page().click_login()
-        self.pagefactory.login_page().login_func('17863272967', 'miao1998')
+        """登录测试方法"""
+        # 成功登录
+        # self.page_factory.index_page().click_mine()  # 点击我的
+        # self.page_factory.mine_page().click_login()  # 点击登录/注册
+        # self.page_factory.login_page().login_func('13157523435', 'lm123456')  # 登录
+        # self.page_factory.login_page().click_confirm_btn() # 点击登录提示确定按钮
+        # # 断言
+        # username_text = self.page_factory.mine_page().get_username_text()
+        # assert '3435' in username_text
 
-        username_text = self.pagefactory.mine_page().get_username_text()
-        assert '2967' in username_text
+        # 未注册
+        self.pagefactory.index_page().click_mine()  # 点击我的
+        self.pagefactory.mine_page().click_login()  # 点击登录/注册
+        self.pagefactory.login_page().login_func('17800001111', 'miao1998')  # 登录
 
-    # 错误实例化思路
-    # def index_page(self):  # 首页
-    #
-    #
-    # def mine_page(self):  # 我的页面
-    #     pass
-    #
-    # def login_page(self):  # 登录页面
-    #     pass
+        try:
+            # 断言
+            toast_msg = self.pagefactory.login_page().get_login_toast('账号还未注册')
+            assert '账号还未注册' in toast_msg
+
+        except AssertionError as e:  # AssertionError: 为断言失败异常类型
+
+            # 给 allure 报告添加截图(断言失败时进行截图操作)
+            self.driver.get_screenshot_as_file('./screenshot/info.png')
+            # rb: 以二进制方式读取
+            with open('./screenshot/info.png', 'rb') as f:
+                # allure.MASTER_HELPER.attach('文件名称', 文件内容, 文件类型)
+                allure.MASTER_HELPER.attach('my_info', f.read(), allure.MASTER_HELPER.attach_type.PNG)
+
+            raise e  # 当截图操作完成后, 应该还原测试用例的真实结果, 因此需要再主动抛出异常
